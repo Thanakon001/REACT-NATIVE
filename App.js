14ece0e2ffad1f69b2dc8c25e57bf8504a55d511
Thanakon001 <148675451+Thanakon001@users.noreply.github.com>
2024-12-03 09:00:10 +0700

    moblieAppp

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import NavigationBar from "./components/NavigationBar";
+import BodyContainer from "./components/BodyContainer";
+
+
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationBar />
+      <BodyContainer />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    rowGap: 15,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 });
